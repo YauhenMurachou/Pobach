7f4d64f5c13f079b4527cd2f3cbb663361274a61
YauhenMurachou <emurach1992@gmail.com>
2022-05-10 23:57:15 +0300

    77 lesson. Added red border and warning when we have error

--- src/components/common/FormsControl.js
+++ src/components/common/FormsControl.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
-export const Textarea = ({input, meta, ...props}) => {
+import classes from './FormsControl.module.css';
+
+export const Textarea = ({ input, meta, ...props }) => {
+
+	const hasError = meta.touched && meta.error;
+
 	return (
-		<div>
-			<textarea {...input} {...props} />
+		<div className={classes.formControl + ' ' + (hasError ? classes.error : '')}>
+			<div>
+				<textarea {...input} {...props} />
+			</div>
+			<div>
+				{hasError && <span>{meta.error}</span>}
+			</div>
 		</div>
 	)
 }
