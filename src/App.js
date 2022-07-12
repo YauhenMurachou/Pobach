8d129452717ba81fb1b75db7b97a8f46c575baf5
YauhenMurachou <emurach1992@gmail.com>
2022-07-12 23:08:34 +0300

    feat: add basename in BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,53 +1,52 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect } from "react"
+import { BrowserRouter, Route } from "react-router-dom"
+import { useDispatch, useSelector } from "react-redux"
 
-import HeaderContainer from './components/header/HeaderContainer';
-import Navbar from './components/navbar/Navbar';
-import ProfileContainer from './components/profile/ProfileContainer';
-import DialogsContainer from './components/dialogs/DialogsContainer';
-import Feed from './components/feed/Feed';
-import Photos from './components/photos/Photos';
-import Settings from './components/settings/Settings';
-import UsersContainer from './components/users/UsersContainer';
-import Login from './components/login/Login';
-import { initializedThunkCreator } from './redux/appReducer';
-import Loader from './components/loader/Loader';
+import HeaderContainer from "./components/header/HeaderContainer"
+import Navbar from "./components/navbar/Navbar"
+import ProfileContainer from "./components/profile/ProfileContainer"
+import DialogsContainer from "./components/dialogs/DialogsContainer"
+import Feed from "./components/feed/Feed"
+import Photos from "./components/photos/Photos"
+import Settings from "./components/settings/Settings"
+import UsersContainer from "./components/users/UsersContainer"
+import Login from "./components/login/Login"
+import { initializedThunkCreator } from "./redux/appReducer"
+import Loader from "./components/loader/Loader"
 
-
-import './App.css';
+import "./App.css"
 
 const App = () => {
+  const dispatch = useDispatch()
+  const { isInitialized } = useSelector((state) => state.appReducer)
 
-	const dispatch = useDispatch();
-	const { isInitialized } = useSelector(state => state.appReducer);
+  useEffect(() => {
+    dispatch(initializedThunkCreator())
+  }, [])
 
-	useEffect(() => {
-		dispatch(initializedThunkCreator())
-	}, []);
-
-	return (
-		<div>
-			{isInitialized && <BrowserRouter>
-				<div className='app-wrapper'>
-					<HeaderContainer />
-					<Navbar />
-					<div className='app-wrapper-content'>
-						<Route path='/Dialogs' render={() => <DialogsContainer />} />
-						<Route path='/Profile/:userId?' render={() => <ProfileContainer />} />
-						<Route path='/Feed' component={Feed} />
-						<Route path='/Photos' component={Photos} />
-						<Route path='/Settings' component={Settings} />
-						<Route path='/Users' render={() => <UsersContainer />} />
-						<Route path='/Login' render={() => <Login />} />
-					</div>
-				</div>
-			</BrowserRouter>}
-			{!isInitialized && <Loader isFetching={!isInitialized} />}
-		</div>
-
-
-	);
+  return (
+    <div>
+      {isInitialized && (
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
+          <div className="app-wrapper">
+            <HeaderContainer />
+            <Navbar />
+            <div className="app-wrapper-content">
+              <Route path="/Dialogs" render={() => <DialogsContainer />} />
+              <Route path="/Profile/:userId?" render={() => <ProfileContainer />} />
+              <Route path="/Feed" component={Feed} />
+              <Route path="/Photos" component={Photos} />
+              <Route path="/Settings" component={Settings} />
+              <Route path="/Users" render={() => <UsersContainer />} />
+              <Route path="/Login" render={() => <Login />} />
+            </div>
+          </div>
+        </BrowserRouter>
+      )}
+      {!isInitialized && <Loader isFetching={!isInitialized} />}
+    </div>
+  )
 }
 
-export default App;
+export default App
+
