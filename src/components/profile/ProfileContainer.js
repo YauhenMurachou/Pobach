4a7aa6fcdca9ec4f7ccc881385fdce48b5df9bde
YauhenMurachou <emurach1992@gmail.com>
2022-07-14 23:08:21 +0300

    fix: add ComponentDidUpdate in ProfileContainer. Redirect on my profile

--- src/components/profile/ProfileContainer.js
+++ src/components/profile/ProfileContainer.js
@@ -1,48 +1,60 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { compose } from 'redux';
-import { withRouter } from 'react-router-dom';
+import React from "react"
+import { connect } from "react-redux"
+import { compose } from "redux"
+import { withRouter } from "react-router-dom"
 
-import Profile from './Profile';
-import { setUserProfileThunkCreator, getStatusThunkCreator, updateStatusThunkCreator } from '../../redux/profilePageReducer';
-import withAuthRedirect from '../hoc/withAuthRedirect';
+import Profile from "./Profile"
+import {
+  setUserProfileThunkCreator,
+  getStatusThunkCreator,
+  updateStatusThunkCreator
+} from "../../redux/profilePageReducer"
+import withAuthRedirect from "../hoc/withAuthRedirect"
 
 class ProfileContainer extends React.Component {
+  refreshProfile() {
+    let userId = this.props.match.params.userId
+    if (!userId) {
+      userId = this.props.userId
+    }
+    this.props.setUserProfile(userId)
+    this.props.getStatus(userId)
+  }
 
-	
-	componentDidMount() {
+  componentDidMount() {
+    // this.props.setMyProfile()
+    // console.log('ProfileContainer', this.props)
+    this.refreshProfile()
+  }
 
-		// console.log('ProfileContainer', this.props)
-		// this.props.setMyProfile()
-		// console.log('ProfileContainer', this.props)
-		let userId = this.props.match.params.userId;
-		if (!userId) {
-			userId = this.props.userId
-		}
-		this.props.setUserProfile(userId)
-		this.props.getStatus(userId)
-	}
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (this.props.match.params.userId !== prevProps.match.params.userId) {
+      this.refreshProfile()
+    }
+  }
 
-	render() {
-		return <>
-			<Profile {...this.props} profile={this.props.profile} />
-		</>
-	}
-};
+  render() {
+    return (
+      <>
+        <Profile {...this.props} profile={this.props.profile} />
+      </>
+    )
+  }
+}
 
 let mapStateToProps = (state) => ({
-	profile: state.profilePage.profile,
-	status: state.profilePage.status,
-	userId: state.auth.userId
+  profile: state.profilePage.profile,
+  status: state.profilePage.status,
+  userId: state.auth.userId
 })
 
 export default compose(
-	connect(mapStateToProps, {
-		setUserProfile: setUserProfileThunkCreator,
-		getStatus: getStatusThunkCreator,
-		updateStatus: updateStatusThunkCreator
-		// setMyProfile: setUserDataThunkCreator
-	}),
-	withRouter,
-	withAuthRedirect)(ProfileContainer);
-
+  connect(mapStateToProps, {
+    setUserProfile: setUserProfileThunkCreator,
+    getStatus: getStatusThunkCreator,
+    updateStatus: updateStatusThunkCreator
+    // setMyProfile: setUserDataThunkCreator
+  }),
+  withRouter,
+  withAuthRedirect
+)(ProfileContainer)
--- src/components/profile/myposts/post/profileInfo/ProfileInfo.js
+++ src/components/profile/myposts/post/profileInfo/ProfileInfo.js
@@ -1,40 +1,43 @@
-import React from 'react';
+import React from "react"
 
-import Loader from '../../../../loader/Loader';
-import ProfileStatus from './profilleStatus/ProfileStatus';
-import avatar from '../../../../../images/avatar.png';
+import Loader from "../../../../loader/Loader"
+import ProfileStatus from "./profilleStatus/ProfileStatus"
+import avatar from "../../../../../images/avatar.png"
 
-import classes from './ProfileInfo.module.css'
+import classes from "./ProfileInfo.module.css"
 
 const ProfileInfo = (props) => {
+  if (!props.profile) {
+    return <Loader />
+  }
 
-	if (!props.profile) {
-		console.log('ProfileInfo inside--', props)
-		return <Loader />
-	}
+  let objProp = props.profile
+  let info = Object.getOwnPropertyNames(objProp)
 
-	let objProp = props.profile;
-	let info = Object.getOwnPropertyNames(objProp);
+  return (
+    <>
+      <div>
+        <img alt="photos" src={props.profile.photos.large || avatar} />
+      </div>
 
-	return <>
-		<div>
-			<img alt='photos' src={props.profile.photos.large ? props.profile.photos.large : avatar} />
-		</div>
+      {info.map((item, index) => {
+        if (objProp[item] && typeof objProp[item] !== "object") {
+          return (
+            <div key={index}>
+              {" "}
+              {item}: {objProp[item]}{" "}
+            </div>
+          )
+        } else {
+          return <div key={index}> {item}: No data </div>
+        }
+      })}
 
-		{info.map((item, index) => {
-			if (objProp[item] && typeof (objProp[item]) !== 'object') {
-				return <div key={index}> {item}: {objProp[item]} </div>
-			} else {
-				return <div key={index}> {item}: No data </div>
-			}
-		})}
+      <div className={classes.item}>
+        <ProfileStatus status={props.status} updateStatus={props.updateStatus} />
+      </div>
+    </>
+  )
+}
 
-		<div className={classes.item}>
-			<ProfileStatus status={props.status} updateStatus={props.updateStatus} />
-		</div>
-	</>
-};
-
-export default ProfileInfo;
-
-
+export default ProfileInfo
