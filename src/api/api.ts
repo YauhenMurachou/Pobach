715589ae780cc74bac7d4fa6e3543209f2a50572
YauhenMurachou <emurach1992@gmail.com>
2023-01-31 22:08:12 +0300

    TASK-046 Use Redux-toolkit

--- src/api/api.ts
+++ src/api/api.ts
@@ -102,3 +102,12 @@ export const registrationApi = {
     return response.data;
   },
 };
+
+export const photosApi = {
+  async getPhotos() {
+    const response = await fetch(
+      'https://jsonplaceholder.typicode.com/albums/1/photos'
+    );
+    return response.json();
+  },
+};
--- src/components/photos/Photos.tsx
+++ src/components/photos/Photos.tsx
@@ -1,16 +1,36 @@
 import { FC } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Redirect } from 'react-router-dom';
+import { fetchPhotos } from 'src/redux/photosReducer';
 import { RootState } from 'src/redux/redux-store';
+
+import styles from './Photos.module.css';
 
 const Photos: FC = () => {
   const { isAuth } = useSelector((state: RootState) => state.auth);
+  const photos = useSelector((state: RootState) => state.photos.photos);
+  const dispatch = useDispatch();
+
+  const clickForPhotos = () => {
+    dispatch(fetchPhotos());
+  };
   if (!isAuth) {
     return <Redirect to="/Login" />;
   }
   return (
     <div>
-      <div>Photo</div>
+      <h2>Photos</h2>
+      <div>This is test page for practice with redux-toolkit</div>
+      <button type="button" onClick={clickForPhotos}>
+        Get photos
+      </button>
+      <div className={styles.container}>
+        {photos.map((photo) => (
+          <div>
+            <img key={photo.id} src={photo.thumbnailUrl} alt={photo.title} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
