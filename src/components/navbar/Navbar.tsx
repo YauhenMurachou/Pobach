bec06bf1d4d5a385ab4a1dbcadd7f70ac0737267
YauhenMurachou <emurach1992@gmail.com>
2023-04-03 23:35:28 +0400

    TASK-041 CSS variables

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -18,6 +18,7 @@ const Navbar: FC = () => {
           to={index === 0 ? profilePath : path}
           activeClassName={classes.activeLink}
           className={classes.item}
+          key={path + text}
         >
           {icon}
           {text}
