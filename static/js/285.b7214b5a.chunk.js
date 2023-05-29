7f8ce8eb5cbc6b08485bb7aba19bb9e480156aa6
YauhenMurachou <emurach1992@gmail.com>
2023-05-29 19:57:01 +0400

    Updates

--- static/js/285.b7214b5a.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkmy_app=self.webpackChunkmy_app||[]).push([[285],{3285:function(t,n,u){u.r(n);var e=u(364),r=u(8931),a=u(184);n.default=function(){var t=(0,e.v9)((function(t){return t.auth})),n=t.isAuth,u=t.userId;return n?n?(0,a.jsx)(r.l_,{to:"/Profile/".concat(u)}):(0,a.jsx)(a.Fragment,{}):(0,a.jsx)(r.l_,{to:"/Login"})}}}]);
-//# sourceMappingURL=285.b7214b5a.chunk.js.map
